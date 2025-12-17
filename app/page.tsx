25ace12cc23b3139be2fc0c8cf83dbd80b846604
matmaz99 <62934060+matmaz99@users.noreply.github.com>
2025-12-17 14:29:37 +0100

    Update main page with all sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,23 @@
 import { Hero } from '@/components/sections/hero'
 import { ProblemSolution } from '@/components/sections/problem-solution'
+import { Approach } from '@/components/sections/approach'
+import { About } from '@/components/sections/about'
+import { Services } from '@/components/sections/services'
+import { FAQ } from '@/components/sections/faq'
+import { Booking } from '@/components/sections/booking'
+import { Footer } from '@/components/sections/footer'
 
 export default function Home() {
   return (
     <main className="min-h-screen">
       <Hero />
       <ProblemSolution />
+      <Approach />
+      <About />
+      <Services />
+      <FAQ />
+      <Booking />
+      <Footer />
     </main>
   )
 }
